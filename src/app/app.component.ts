68c3bca908172ca365eec672be4580f819b4df39
Vasyl Bomba <bombavasyl@gmial.com>
2022-09-07 12:22:43 +0200

    clock added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'yura-project';
+  title = 'clock-greets';
+  time: any;
+  hours: any;
+  msg: any;
+  link: any;
+  constructor() {
+    setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+
+    this.decide();
+  }
+
+  decide() {
+    this.hours = new Date().getHours();
+    console.log('this.hours', this.hours);
+    if (this.hours < 10) {
+      this.msg = 'Good Morning';
+      this.link = 'wwww.google.com';
+      console.log('selamat Pagi');
+    } else if (this.hours < 16) {
+      this.msg = 'Good Afternoon';
+      this.link = 'wwww.tokopedia.com';
+      console.log('selamat siang');
+    } else if (this.hours < 19) {
+      this.msg = 'Good Evening';
+    } else if (this.hours < 24) {
+      this.msg = 'Good Night';
+      this.link = 'wwww.sprout.co.id';
+      console.log('selamat malam');
+    } else if (this.hours < 6) {
+      this.msg = 'Sleep lah';
+      this.link = 'www.mangabat.com';
+      console.log('selamat subuh');
+    }
+  }
+
+  firebaseSusbscribing() {}
 }
